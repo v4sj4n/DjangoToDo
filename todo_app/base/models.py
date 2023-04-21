95d3c532b076f043a6b896fe6fb92f93df242daa
Vasjan Çupri <vasjancupri04@gmail.com>
2023-04-21 19:13:11 +0200

    Added a model and static files (css)

--- todo_app/base/models.py
+++ todo_app/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_completed = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+    
